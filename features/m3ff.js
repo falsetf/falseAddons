f635e6400b5101c9e2719e26018fc7e7619a43cd
falsetf <falsetf2@protonmail.com>
2024-11-27 07:40:29 -0600

    function over form

--- features/m3ff.js
+++ features/m3ff.js
@@ -2,25 +2,10 @@ import Settings from "../config.js"
 
 register("chat", ()=> {
     if (!Settings().m3ff) return;
+    World.playSound("random.orb", 2, 1);
+    Client.showTitle("&fGET READY", "", 0, 10, 0);
     setTimeout(() => {
         World.playSound("random.orb", 2, 1);
-        Client.showTitle("&44", "", 0, 10, 0);
-        setTimeout(() => {
-            World.playSound("random.orb", 2, 1);
-            Client.showTitle("&43", "", 0, 10, 0);
-            setTimeout(() => {
-                World.playSound("random.orb", 2, 1);
-                Client.showTitle("&42", "", 0, 10, 0);
-                setTimeout(() => {
-                    World.playSound("random.orb", 2, 1);
-                    Client.showTitle("&41", "", 0, 10, 0);
-                    setTimeout(() => {
-                        World.playSound("random.anvil_land", 2, 1);
-                        Client.showTitle("&fFIRE FREEZE", "", 0, 20, 0);
-                    }, 1000);
-                }, 1000);
-            }, 1000);
-        }, 1000);
-    }, 1000);
-    
-}).setChatCriteria("[BOSS] The Professor: Oh? You found my Guardians' one weakness?").setContains();
+        Client.showTitle("&4FIRE FREEZE", "", 0, 20, 0);
+    }, 2000);
+}).setChatCriteria("[BOSS] The Professor: Even if you took my barrier down, I can still fight.").setContains();
